1c229ae158f3000650c840fe867d063f6ddd74ba
Mohamed Nazir <mohamednazirm686@gmail.com>
2026-05-10 00:12:28 -0400

    Optimize smooth scroll function with block: 'start' option

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,10 @@ export default function App() {
   const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId)
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' })
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
     }
   }
 
